b0e7b7efc9c93cfc178169e9ae3ed270a13af9e9
fatimahproject <fatimah140903@gmail.com>
2023-07-04 11:01:46 +0700

    Menjalankan Program

--- src/kampus/Dosen.java
+++ src/kampus/Dosen.java
@@ -1,5 +1,20 @@
 package kampus;
 
 public class Dosen {
+    //membuat variabel
+    private double Nidn, Nama, MataKuliah;
+    
+    //membuat constructor
+
+    public Dosen(double nidn, double nama, double mataKuliah) {
+        this.Nidn = nidn;
+        this.Nama = nama;
+        this.MataKuliah = mataKuliah;
+    }
+    
+    //membuat method
+    public double setNidn() {
+        return Nidn;
+    }
     
 }
